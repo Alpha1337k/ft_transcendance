522627e7b8c4c55fe461adebb64dfc722b457a23
Alpha1337k <ozziekruithof@gmail.com>
2021-08-17 16:23:24 +0200

    Feat: added 2fa module

---
+++ src/src/modules/2fa/2fa.ts
@@ -0,0 +1,48 @@
+import * as speakeasy from 'speakeasy';
+import * as qrcode from 'qrcode';
+import * as readline from 'readline'
+
+var secrets = [];
+
+function getTwoFactorAuthenticationCode() {
+	const secretCode = speakeasy.generateSecret({name: "Pong online"});
+	return {
+		otpauthUrl : secretCode.otpauth_url,
+		base32: secretCode.base32,
+	};
+}
+
+function createQrCodeAsImg(url:string) {
+	qrcode.toFile("img.png", url, function (err) {
+		console.log("error", err);
+	})
+}
+
+function check2faInput(input: string, secret : string)
+{
+	var check = speakeasy.totp.verify({secret : secret, encoding: "base32", token : input});
+	console.log("check returns", check);
+	return (check);
+}
+
+function UserInput(query: string) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    return new Promise(resolve => rl.question(query, ans => {
+        rl.close();
+        resolve(ans);
+    }))
+}
+
+async function runexample() {
+	const data = getTwoFactorAuthenticationCode();
+	console.log(data);
+	secrets.push(data.base32);
+	createQrCodeAsImg(data.otpauthUrl);
+	while (1) {
+		check2faInput(await UserInput("give me the input fam: ") as string, data.base32);
+	}
+}
