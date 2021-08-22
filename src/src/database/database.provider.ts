938fbfac739d80aafe0f704e4af7568f4f56715b
Alpha1337k <ozziekruithof@gmail.com>
2021-08-22 22:39:35 +0200

    Feat: added database provider, to be used in other classes

---
+++ src/src/database/database.provider.ts
@@ -0,0 +1,19 @@
+import { createConnection } from 'typeorm';
+
+export const databaseProvider = [
+	{
+	  provide: 'DATABASE_CONNECTION',
+	  useFactory: async () => await createConnection({
+		type: 'postgres',
+		host: 'localhost',
+		port: 5432,
+		username: 'postgres',
+		password: 'codam',
+		database: 'test',
+		entities: [
+			__dirname + '/../**/*.entity{.ts,.js}',
+		],
+		synchronize: true,
+	  }),
+	},
+  ];
