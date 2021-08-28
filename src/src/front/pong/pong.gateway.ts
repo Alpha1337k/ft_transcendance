0d6d0721028604c66d3916b2a63504a5f26b5c00
Alpha1337k <ozziekruithof@gmail.com>
2021-08-28 22:06:40 +0200

    Chore: tabs

--- src/src/front/pong/pong.gateway.ts
+++ src/src/front/pong/pong.gateway.ts
@@ -12,50 +12,42 @@ import {
   
   @WebSocketGateway()
   export class PongGateway
-	  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
-	  @WebSocketServer() server: Server;
-  
-		  constructor(private readonly appService : PongService) {}
-  
-	  private logger: Logger = new Logger('PongGateway');
-  
-	  
-	  @SubscribeMessage('sendClientUpdate')
-	  handleUpdate(client: Socket, payload: {id: string , updt : positionUpdate}): void {
-		  console.log("Update!:", payload);
-		  this.appService.updateGame(payload);
-	  }
-	  
-	  @SubscribeMessage('getGameData')
-	  handleData(client: Socket, payload: string) {
-		  console.log("GameData!:", payload);
-		  this.server.to(client.id).emit("getGameData", this.appService.getGameData(payload));
-	  }
-  
-	  @SubscribeMessage('findGame')
-	  findGame(client: Socket, payload : string | any) {
-		  this.appService.addToQueue(client, this.server);
-	  }
-	  
-	  
-	  
-	  afterInit(server: Server) {
-		  this.logger.log('Init');
-	  }
-	  
-	  handleDisconnect(client: Socket) {
-		  this.logger.log(`Client disconnected: ${client.id}`);
-	  }
-	  
-	  handleConnection(client: Socket, ...args: any[]) {
-		  this.logger.log(`Client connected: ${client.id}`);
-	  }
-  
-  
-	  @SubscribeMessage('msgToServer')
-	  handleMessage(client: Socket, payload: string): void {
-		  console.log(`Message: `, payload);
-		  this.server.emit('msgToClient', payload);
-	  }
-  }
-  
+	implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+	@WebSocketServer() server: Server;
+
+		constructor(private readonly appService : PongService) {}
+
+	private logger: Logger = new Logger('PongGateway');
+
+
+	@SubscribeMessage('sendClientUpdate')
+	handleUpdate(client: Socket, payload: {id: string , updt : positionUpdate}): void {
+		console.log("Update!:", payload);
+		this.appService.updateGame(payload);
+	}
+
+	@SubscribeMessage('getGameData')
+	handleData(client: Socket, payload: string) {
+		console.log("GameData!:", payload);
+		this.server.to(client.id).emit("getGameData", this.appService.getGameData(payload));
+	}
+
+	@SubscribeMessage('findGame')
+	findGame(client: Socket, payload : string | any) {
+		this.appService.addToQueue(client, this.server);
+	}
+
+
+
+	afterInit(server: Server) {
+		this.logger.log('Init');
+	}
+
+	handleDisconnect(client: Socket) {
+		this.logger.log(`Client disconnected: ${client.id}`);
+	}
+
+	handleConnection(client: Socket, ...args: any[]) {
+		this.logger.log(`Client connected: ${client.id}`);
+	}
+}
