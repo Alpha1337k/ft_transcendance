665375db30ff8ba5ab739aff9753c8af876a3f0f
Alpha1337k <ozziekruithof@gmail.com>
2021-09-09 20:32:14 +0200

    Fix: double socket connect && Chore: css chess

--- src/site_static/chat.js
+++ src/site_static/chat.js
@@ -4,18 +4,11 @@ function messagesend() {
 	g_socket.emit("sendChatMessage", {userid:1, chatid: 1, message: document.getElementById("mshh").value});
 }
 
-async function connect() {
-	g_socket = io("http://localhost:5000/");
-	
+g_socket.on("connect", () => {
+	console.log("connected!!!");
+});
 
-	g_socket.on("connect", () => {
-		console.log("connected!!!");
-	});
-
-	g_socket.on("msgToClients", data => {
-		console.log("message recieved!!", data);
-		document.getElementsByClassName("messagebox")[0].innerHTML += data;
-	});
-}
-
-connect();
+g_socket.on("msgToClients", data => {
+	console.log("message recieved!!", data);
+	document.getElementsByClassName("messagebox")[0].innerHTML += data;
+});
