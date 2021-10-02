3b7e61bebe5f75b64de3792fd4a4e8884157686d
Alpha1337k <ozziekruithof@gmail.com>
2021-10-02 21:34:14 +0200

    Feat: basic profile render working

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,7 +1,35 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
+
+interface Match {
+	matchid: number;
+	players: User[];
+	p1Score: number;
+	p2Score: number;
+}
+
+enum UserRank {
+	SPLUS = 'S+',
+	S = 'S',
+	A = 'A',
+	B = 'B',
+	C = 'C',
+	D = 'D',
+}
+
+interface User {
+	userid:		string;
+	name:		string;
+	wins:		number;
+	losses: 	number;
+	lastSeen:	Date;
+	history:	Match[];
+	userElo:	number;
+	UserRank:	UserRank;
+}
 
 @Component({
   selector: 'app-profile',
@@ -12,22 +40,51 @@ export class ProfileComponent implements OnInit {
 
 	private routeSub: Subscription | undefined;
 	private profileId: number = 0;
-	profileHTML: string = '';
-  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+	user: User | undefined;
 
-  ngOnInit(): void {
-	this.routeSub = this.route.params.subscribe(params => {
-		console.log(params) //log the entire params object
-		console.log(params['id']) //log the value of id
-		this.profileId = params['id'] as number;
-		console.log(this.profileId);
-		this.loadContent();
-	  });
+	constructor(private route: ActivatedRoute, private http: HttpClient, private sanitizer: DomSanitizer)
+	{
+
+	}
+	
+	async ngOnInit(): Promise<void>
+	{
+		this.routeSub = this.route.params.subscribe(params => {
+			console.log(params) //log the entire params object
+			console.log(params['id']) //log the value of id
+			this.profileId = params['id'] as number;
+			console.log(this.profileId);
+			this.setContent();
+		  });
+	}
+  addUser()
+  {
+	  console.log("aahahhahaha");
   }
 
-  async loadContent(): Promise<void> {
-	let page = await this.http.get(`http://localhost:5000/profile/${this.profileId}`, { responseType: 'text'}).toPromise();
-	this.profileHTML = page;
+  createHistoryDoms(matches: Match[]): string {
+		let rval = '';
+		if (matches == null || matches.length == 0)
+			return '<h3>Nothing to display</h3>';
+		for (let index = 0; index < matches.length; index++) {
+			const m = matches[index];
+			rval += `
+			<div class="past-match">
+				<a routerLink="/profile/1">${"User1"}</a>
+				<p> | </p>
+				<a routerLink="/profile/2">${"User2"}</a>
+				<h5>${m.p1Score} - ${m.p2Score}</h5>
+			</div>`;
+		}
+		return rval;
+	}
+
+
+  async setContent(): Promise<void> {
+	  console.log('adding user or smth');
+	let usr: User = await this.http.get<User>(`http://localhost:5000/profile/get/${this.profileId}`).toPromise();
+	console.log(usr);
+	this.user = usr;
   }
 
   ngOnDestroy(): void {
